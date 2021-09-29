d50321194af1212b2164e708c7ae993c1fc4d366
duc08 <duc08@DESKTOP-NVG2TNG>
2021-09-29 23:14:23 +0700

    server

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '530229fb505e06a72c64ca31d99ecccc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fae60075-8aa0-48cc-9185-319da495d135'
+};
